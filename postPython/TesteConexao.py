91acb6af09c0cabc357dda49cb08b6429298ff9e
Abdias Viana <abdviana@gmail.com>
2019-01-04 10:35:38 -0300

    ...

--- postPython/TesteConexao.py
+++ postPython/TesteConexao.py
@@ -3,14 +3,14 @@ from Conexao import *
 
 con = Conexao("pq://abdiassantos:Voljin!555@localhost/postpython")
 
-opcao = int(1)
+opcao = int(2)
 
 if opcao == 1:
     sql = "insert into cidade values (default, 'Bahia', 'BA')"
     if con.manipular(sql):
         print('Inserido com sucesso!')
 if opcao == 2:
-    sql = "delete from cidade where id = 2"
+    sql = "delete from cidade where id = 8"
     if con.manipular(sql):
         print('Deletado com sucesso!')
 print(con.proximaPK('cidade', 'id'))
